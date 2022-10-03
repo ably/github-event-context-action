eb7a8c6bb0948e4c2ca444636a43ff9022dc843e
Quintin Willison <q@qwuk.net>
2022-10-03 09:44:36 +0100

    Rename description to title.
    The new name is more appropriate for how it's used downstream.

--- server.js
+++ server.js
@@ -47,16 +47,16 @@ const ref = createRef();
 
 let { sha } = context;
 let url = `https://github.com/${context.repo.owner}/${context.repo.repo}/`;
-let description;
+let title;
 if (context.eventName === 'pull_request') {
   sha = githubEvent.pull_request.head.sha;
   url += `pull/${githubEvent.pull_request.number}/`;
-  description = `Pull Request #${githubEvent.pull_request.number}`;
+  title = `Pull Request #${githubEvent.pull_request.number}`;
 } else if (context.eventName === 'push' && ref !== null && ref.type === 'head' && ref.name === 'main') {
-  description = 'Default Branch';
+  title = 'Default Branch';
 } else if (context.eventName === 'push' && ref !== null && ref.type === 'tag') {
   url += `releases/tag/${ref.name}/`;
-  description = `Tag: ${ref.name}`;
+  title = `Tag: ${ref.name}`;
 } else {
   core.setFailed("Error: this action can only be ran on a pull_request, a push to the 'main' branch, or a push of a tag");
   process.exit(1);
@@ -69,4 +69,4 @@ const emit = (key, value) => {
 
 emit('sha', sha);
 emit('url', url);
-emit('description', description);
+emit('title', title);
